01a77d2c1577dbd45115a8a310870569247bfd33
OlavOlsm <OlavOlsm@gmail.com>
2019-03-11 19:20:25 +0100

    Remove class_exists in poloniex override This is no longer needed after autoload of exchanges in cryptowoo

--- exchanges/class.exchange-poloniex-bch.php
+++ exchanges/class.exchange-poloniex-bch.php
@@ -3,23 +3,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }// Exit if accessed directly
 
-if ( ! class_exists( CW_Exchange_Poloniex_BCH::class ) ) {
-	/**
-	 * Poloniex Exchange Rates Class
+/**
+ * Poloniex Exchange Rates Class
+ *
+ * @category CryptoWoo
+ * @package Exchange
+ * @subpackage ExchangeBase
+ * Author: We Program IT | legal company name: OS IT Programming AS | Company org nr: NO 921 074 077
+ * Author URI: https://weprogram.it
+ */
+class CW_Exchange_Poloniex_BCH extends CW_Exchange_Poloniex {
+	/** Get the exchange rate pair (base/currency)
 	 *
-	 * @category CryptoWoo
-	 * @package Exchange
-	 * @subpackage ExchangeBase
-	 * Author: We Program IT | legal company name: OS IT Programming AS | Company org nr: NO 921 074 077
-	 * Author URI: https://weprogram.it
+	 * @return string
 	 */
-	class CW_Exchange_Poloniex_BCH extends CW_Exchange_Poloniex {
-		/** Get the exchange rate pair (base/currency)
-		 *
-		 * @return string
-		 */
-		protected function get_search_pair() : string {
-			return str_replace( 'BCH', 'BCHABC', parent::get_search_pair() );
-		}
+	protected function get_search_pair() : string {
+		return str_replace( 'BCH', 'BCHABC', parent::get_search_pair() );
 	}
 }
